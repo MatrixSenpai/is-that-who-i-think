19c6d3c8c91a6ef691e451edcd36ee3b48ce1594
MatrixSenpai <math.matrix@icloud.com>
2020-06-09 15:57:10 -0500

    Initial Commit

---
+++ ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by Mason Phillips on 6/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
